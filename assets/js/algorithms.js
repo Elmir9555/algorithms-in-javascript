cb2c7d61c5da7c7dc4e6575d985f20a08a3419eb
Elmir9555 <elmiriu@code.edu.az>
2022-01-30 12:57:23 +0400

    issues of algorithms

---
+++ assets/js/algorithms.js
@@ -0,0 +1,62 @@
+//aylarin neçənci ay olduğunun tapılması
+
+
+// let months = ["Yanvar", "Fevral", "Mart", "Aprel"]
+
+// function countMonth(array) {
+//     let count = [];
+//     for (let i = 0; i < array.length; i++) {
+//         let number = i + 1;
+//         let result = months[i] + "-" + number
+//         count.push(result)
+
+//     }
+//     console.log(count);
+
+// }
+
+// countMonth(months)
+
+
+
+
+
+
+
+
+//ədədlərin böyükdən kiçiyə ardıcıllığı
+
+// let arr=[1,4,7,2,9,6,5]
+// arr.sort();
+// arr.reverse()
+
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+//functiona string elave olunması
+
+
+// let str="salam olsun özünü tanıyan oğlanlara"
+
+// function stringArray(string) {
+//     let arr=string.split(" ")
+    
+//     let newArr=arr.map(m=>m.length)
+//     console.log(newArr);
+       
+// }
+
+// stringArray(str);
+
+
+
+
+
